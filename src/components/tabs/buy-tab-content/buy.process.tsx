e19c9a1534b4da9f8ae451a2ce27d2aaba3fed09
David May <david.leo.may@gmail.com>
2023-07-05 17:43:59 +0200

    [NO-TASK] Update to newest package

--- src/components/tabs/buy-tab-content/buy.process.tsx
+++ src/components/tabs/buy-tab-content/buy.process.tsx
@@ -22,7 +22,7 @@ import useDebounce from '../../../hooks/debounce.hook';
 import { BuyCompletion } from '../../buy/buy-completion';
 import { PaymentInformation, PaymentInformationContent } from '../../buy/payment-information';
 import { KycHint } from '../../kyc-hint';
-import { Asset, AssetType, Buy, Fiat, Utils, Validations, useBuyContext, useFiat } from '@dfx.swiss/react';
+import { Asset, AssetType, Buy, Fiat, Utils, Validations, useBuy, useFiat } from '@dfx.swiss/react';
 
 interface BuyTabContentProcessProps {
   asset?: Asset;
@@ -36,7 +36,7 @@ interface FormData {
 }
 
 export function BuyTabContentProcess({ asset, onBack }: BuyTabContentProcessProps): JSX.Element {
-  const { currencies, receiveFor } = useBuyContext();
+  const { currencies, receiveFor } = useBuy();
   const { isAllowedToBuy } = useKycHelper();
   const { toDescription, toSymbol } = useFiat();
   const [paymentInfo, setPaymentInfo] = useState<PaymentInformation>();
